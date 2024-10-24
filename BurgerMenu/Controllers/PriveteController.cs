32e8493b60afa1200c8bf40d76e719805d70ab68
emredaglar <[emre.daglar1@gmail.com]>
2024-10-24 12:42:55 +0300

    #

---
+++ BurgerMenu/Controllers/PriveteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BurgerMenu.Controllers
+{
+    public class PriveteController : Controller
+    {
+        // GET: Privete
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
