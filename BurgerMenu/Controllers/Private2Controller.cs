c8e092ac0aa7840bf448e5eccc3497c000e72df6
emredaglar <[emre.daglar1@gmail.com]>
2024-10-24 12:47:42 +0300

    #

---
+++ BurgerMenu/Controllers/Private2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BurgerMenu.Controllers
+{
+    public class Private2Controller : Controller
+    {
+        // GET: Private2
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
